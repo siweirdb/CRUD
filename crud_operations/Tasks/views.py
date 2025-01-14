97e8e791665391338fed607cd67ec87c696192f2
Adilet <119230294+siweirdb@users.noreply.github.com>
2025-01-14 19:25:31 +0500

    write crud operations in views file

--- crud_operations/Tasks/views.py
+++ crud_operations/Tasks/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailView(APIView):
+    def get(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = Task.objects.get(pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
