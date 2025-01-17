60ba867250dc2a30be5b1ee8c3acf01d0f26edbc
Adilet <119230294+siweirdb@users.noreply.github.com>
2025-01-17 23:45:43 +0500

    a
    a

--- crud_operations/crud_operations/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from Tasks.views import TaskView, TaskDetailView
-
-urlpatterns = [
-    path('tasks/', TaskView.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-]
